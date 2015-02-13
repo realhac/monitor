572aec85651bcaa1d77da6735b4e300581a18919
unknown <zhangwei19@BJYF-ZHANGWEI19.360buyAD.local>
2015-02-13 11:58:46 +0800

    udpate from office

---
+++ report/report.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+
+import os, sys
+
+class Test_Report:
+    def __init__(self, tr_name):
+        self._html = open(tr_name, 'w')
+
+    def write_tag(self, tag, begin, para=[]):
+        if begin:
+            if para:
+                for pa in para:
+                    self._html.write("<" + tag + " " + pa + " " + ">")
+            else:
+                self._html.write("<" + tag + ">")
+        else:
+            self._html.write("<" + tag + "/>")
+
+    def tr_finish(self):
+        self._html.close()
+
+if __name__ == "__main__":
+    tr = Test_Report("test.html")
+
+    tr.write_tag("html", True)
+    tr.write_tag("head", True)
+    tr.write_tag("title", True)
+    tr.write_tag("title", False)
+    tr.write_tag("head", False)
+    tr.write_tag("body", True)
+    tr.write_tag("body", False)
+    tr.write_tag("html", False)
+
+    tr.tr_finish()
