44c20e840f422dbeb435b858fa91898e4c4f92ec
zhangwei <zhangwei19@jd.com>
2015-03-16 16:43:04 +0800

    update cookie and headers

--- core/ReqResp.py
+++ core/ReqResp.py
@@ -4,19 +4,24 @@ import requests
 import json
 
 class Request_Response:
-    def __init__(self, url, data, headers):
+    def __init__(self, url, data):
         self._url = url
         self._data = data
-        self._headers = headers
+        self._headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
     def postResponse(self):
         r = requests.post(self._url, data = self._data, headers = self._headers)
         return r
 
+    #r.cookies['example_cookie_name']
+    #cookies = dict(cookies_are='working')
+    def postCookieResponse(self, cookies):
+        r = requests.post(self._url, data = self._data, headers = self._headers, cookies=cookies)
+        return r
+
 if __name__ == "__main__":
     u = 'http://ware.m.jd.com/client.action?functionId=skuDyInfo&clientVersion=4.0.0&client=android&osVersion=4.0.3&screen=960*540&networkType=wifi&pin=ljzh347'
     d = 'body={"skuId":"782200"}'
-    h = {"Content-Type": "application/x-www-form-urlencoded"}
-    rr = Request_Response(u, d, h)
+    rr = Request_Response(u, d)
     r = rr.postResponse()
     print r.text
--- core/ReqRespCurl.py
+++ core/ReqRespCurl.py
@@ -4,10 +4,10 @@ import io
 import json
 
 class Request_Response_Curl:
-    def __init__(self, url, data, headers):
+    def __init__(self, url, data):
         self._url = url
         self._data = data
-        self._headers = headers
+        self._headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
     def postResponse(self):
         crl = pycurl.Curl()
@@ -28,13 +28,39 @@ class Request_Response_Curl:
         crl.setopt(pycurl.URL, self._url)
         crl.setopt(crl.WRITEFUNCTION, crl.fp.write)
 
+        crl.perform()
+
+        r = crl.fp.getvalue().decode('utf-8')
+        crl.fp.close()
+
+        return r
+
+    def postCookieResponse(self, cookies):
+        crl = pycurl.Curl()
+        #crl.setopt(pycurl.VERBOSE,1)
+        crl.setopt(pycurl.FOLLOWLOCATION, 1)
+        crl.setopt(pycurl.MAXREDIRS, 5)
+        #crl.setopt(pycurl.AUTOREFERER,1)
+        crl.setopt(pycurl.CONNECTTIMEOUT, 60)
+        crl.setopt(pycurl.TIMEOUT, 300)
+        #crl.setopt(pycurl.PROXY,proxy)
+        #crl.setopt(pycurl.HTTPPROXYTUNNEL,1)
+        #crl.setopt(pycurl.NOSIGNAL, 1)
+        crl.fp = io.BytesIO()
+        crl.setopt(pycurl.USERAGENT, "dhgu hoho")
+
+        # Option -d/--data <data>   HTTP POST data
+        crl.setopt(crl.POSTFIELDS,  self._data)
+        crl.setopt(pycurl.URL, self._url)
+        crl.setopt(crl.WRITEFUNCTION, crl.fp.write)
+
         # Option -b/--cookie <name=string/file> Cookie string or file to read cookies from
         # Note: must be a string, not a file object.
-        crl.setopt(pycurl.COOKIEFILE, "cookie_file_name")
+        crl.setopt(pycurl.COOKIEFILE, cookies)
 
         # Option -c/--cookie-jar <file> Write cookies to this file after operation
         # Note: must be a string, not a file object.
-        crl.setopt(pycurl.COOKIEJAR, "cookie_file_name")
+        crl.setopt(pycurl.COOKIEJAR, cookies)
 
         crl.perform()
 
@@ -46,8 +72,7 @@ class Request_Response_Curl:
 if __name__ == "__main__":
     u = 'http://ware.m.jd.com/client.action?functionId=skuDyInfo&clientVersion=4.0.0&client=android&osVersion=4.0.3&screen=960*540&networkType=wifi&pin=ljzh347'
     d = 'body={"skuId":"782200"}'
-    h = {"Content-Type": "application/x-www-form-urlencoded"}
-    rr = Request_Response_Curl(u, d, h)
+    rr = Request_Response_Curl(u, d)
     r = rr.postResponse()
     #print json.loads(r)
     print r
