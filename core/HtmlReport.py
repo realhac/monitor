d5d844e695636f3fcf966599e00ce62b48fc1f10
zhangwei <zhangwei19@jd.com>
2015-03-18 09:40:53 +0800

    upate

--- core/HtmlReport.py
+++ core/HtmlReport.py
@@ -14,32 +14,54 @@ class HTML_Test_Report:
             else:
                 self._html.write("<" + tag + ">")
         else:
-            self._html.write("<" + tag + "/>")
+            self._html.write("</" + tag + ">")
 
     def trFinish(self):
         self._html.close()
 
-    def writeTableCss(self):
+    def importTableCss(self):
         fp = open("../css/table.css", "rb")
         fc = fp.read();
         self._html.write(fc)
         fp.close()
 
-    def writeTableHead(self):
-        self.writeTag()
+    def drawTableHead(self, head_name):
+        self.writeTag("li", True, ['class="bt"'])
+        self._html.write(head_name)
+        self.writeTag("li", False)
+
+    def drawTableBody(self, cell_name):
+        self.writeTag("li", True)
+        self._html.write(cell_name)
+        self.writeTag("li", False)
+
+    def drawTable(self, cell_name):
+        self.writeTag("div", True, ['id="tb"'])
+        self.writeTag("ul", True)
+        
+        self.drawTableHead(cell_name)
+        self.drawTableHead(cell_name)
+        self.drawTableHead(cell_name)
+        self.drawTableHead(cell_name)
+
+        self.writeTag("ul", False)
+        self.writeTag("div", False)
 
 if __name__ == "__main__":
     tr = HTML_Test_Report("test.html")
 
     tr.writeTag("html", True)
-
-    tr.writeTableCss()
-
     tr.writeTag("head", True)
     tr.writeTag("title", True)
     tr.writeTag("title", False)
+
+    tr.importTableCss()
+
     tr.writeTag("head", False)
     tr.writeTag("body", True)
+
+    tr.drawTable("0")
+
     tr.writeTag("body", False)
     tr.writeTag("html", False)
 
